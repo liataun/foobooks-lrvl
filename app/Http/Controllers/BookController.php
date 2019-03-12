1f3895f7e13530d06c29cb5c63b35ae3f1dccb8e
Brian Twitchell <32440651+liataun@users.noreply.github.com>
2019-03-11 23:47:15 -0400

    Following along with lecture, left in some commented out code

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App;
+
+class BookController extends Controller
+{
+    /**
+     * GET /books
+     */
+    public function index()
+    {
+        # Work that was previously happening in the routes file is now happening here
+        return 'Here are all the books...';
+//        return App::environment();
+    }
+
+    /**
+     * GET /books/{title}
+     */
+    public function show($title)
+    {
+        return 'Results for the book: ' . $title;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,29 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+# New routes
+//Route::get('/example', function () {
+//    return 'hello there!';
+//});
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/books', 'BookController@index');
+
+Route::get('/books/{title}', 'BookController@show');
+
+//Route::get('/books/{title?}', function($title = '') {
+//    if ($title == '') {
+//        return 'Your request did not include a title.';
+//    } else {
+//        return 'Results for the book: '.$title;
+//    }
+//});
+
+//Route::get('/books/{category}/{subcategory}', function($category, $subcategory) {
+//    return 'Here are all the books in the category '.$category.' and '.$subcategory;
+//});
+
+# Existing route
+Route::view('/', 'welcome');
 
 //Route::get('/example', function () {
 //    return view('abc');
