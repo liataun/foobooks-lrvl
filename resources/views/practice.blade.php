cabd59aab9621ea14ecf4513da53b32c830f36dd
Brian Twitchell <32440651+liataun@users.noreply.github.com>
2019-04-21 00:17:59 -0400

    modify practice main page to prompt useful DB link

--- resources/views/practice.blade.php
+++ resources/views/practice.blade.php
@@ -6,6 +6,7 @@
 
 @section('content')
     <h1>Practice</h1>
+    <p>Note to self: Practice 8 selects all rows from Books table and orders by title, so is a good one to use to test database is running correctly.</p>
     @foreach($methods as $method)
         <a href='{{ str_replace('practice', '/practice/', $method) }}'> {{ $method }}</a><br>
     @endforeach
